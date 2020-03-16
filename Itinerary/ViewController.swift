3ce536a8cf04ab4819dc056f1566afb7f2eedd79
ThanhVT <thanh.dtu94@gmail.com>
2020-03-15 20:00:43 -0700

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by ThanhVt on 3/15/20.
+//  Copyright © 2020 ThanhVt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
